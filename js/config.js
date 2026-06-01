35f4ce48df40db818c115378d4c4b33fa8f06696
lucia <157105758+luciney333@users.noreply.github.com>
2026-06-02 00:16:50 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -63,7 +63,7 @@ Ojalá sigamos progresando juntas... 🌸`,
 
   // --- DIFICULTAD DEL JUEGO ---
   // Ajusta cuántos obstáculos hay y su velocidad
- velocidadJuego: 6,
+ velocidadJuego: 9,
  frecuenciaObstaculos: 90,
  maxObstaculos: 5,
 };
